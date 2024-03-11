7f9a5cc6e7124cae097519d0d7685582e135a660
Pleshkov Mihail <pl3shkov.m@gmail.com>
2024-03-11 17:47:48 +0700

    Ch

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
-int main()
+int main(int argc,char *argv[])
 {
-	
+	FILE *rus = fopen(argv[1],"r");
+    FILE *eng = fopen(argv[2],"r");
+
+	if(rus == NULL || eng == NULL){
+        printf("Can't open file\n");
+        return 0;
+    }else if(argc != 3){
+        printf("Error argument\n");
+        return 0;
+    }
+
+    
+
 }
